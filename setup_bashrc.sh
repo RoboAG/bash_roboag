27114eec4e19db21f36d7e7e31c455b8f7da6b1b
Peter Weissig <peterweissig@github.com>
2018-11-17 11:57:24 +0100

    added auto answer "yes"   (mainly for use in scripts)

--- setup_bashrc.sh
+++ setup_bashrc.sh
@@ -4,9 +4,18 @@ echo ""
 echo "setup_bashrc.sh script was called."
 echo "The following project will be sourced within your bashrc."
 echo "    roboag bash scripts"
-echo "Do you want to continue? (y/N)"
-read -s -n 1 ANS; echo ""
-if [ "$ANS" == "y" ]; then
+echo "Do you wish to continue ? (No/yes)"
+if [ "$1" != "-y" ] && [ "$1" != "--yes" ]; then
+    read answer
+else
+    echo "<auto answer \"yes\">"
+    answer="yes"
+fi
+if [ "$answer" != "y" ] && [ "$answer" != "Y" ] && \
+  [ "$answer" != "yes" ]; then
+
+    echo "Your ~./bashrc was NOT changed."
+else
 
     # get local directory
     SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/"
@@ -23,7 +32,4 @@ if [ "$ANS" == "y" ]; then
         echo "# $(date +"%Y_%m_%d") sourcing roboag:"  >> ~/.bashrc
         echo "$BASHRC_SOURCE"                          >> ~/.bashrc
     fi
-else
-
-    echo "Your ~./bashrc was NOT changed."
 fi
