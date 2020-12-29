823530c09c0da0787a018863a922b1ac70e461cd
Peter Weissig <peterweissig@github.com>
2020-12-29 21:33:57 +0100

    updated robo_help_setup_workspace()

--- README.md
+++ README.md
@@ -6,16 +6,31 @@ All scripts are supposed to run on Linux (Ubuntu).
 [![Build Status](https://travis-ci.org/RoboAG/bash_roboag.svg?branch=master)](https://travis-ci.org/RoboAG/bash_roboag)
 
 ## setup workspace
-There are different modes of usage: Either standalone for one user or sharing
-repos with all users.
-
-In **standalone** mode the workspace directory can be freely choosen.
-Nevertheless ''~/workspace/'' is recommended.
+There are two different modes of usage: Either simple mode for a
+single-user-system or sharing repos with all users.
 
 When setting up the scripts for **all users** they will be automatically
-placed in ''/opt/roboag/repos/''.
+placed in ''/opt/roboag/repos/''. This is the default behaviour for our
+clients and servers.
 
-### standalone
+In **single** mode the workspace directory can be freely choosen.
+Nevertheless, ''~/workspace/'' is recommended. This is recommended for
+testing and single-user-systems.
+
+
+For more infos see [help_setup_workspace.sh](scripts/help/help_setup_workspace.sh)
+or just execute:
+
+    robo_help_setup_workspace
+
+### shared repos
+download scripts
+
+    wget -nv https://raw.githubusercontent.com/RoboAG/bash_roboag/master/checkout_all_users.sh
+    source checkout_all_users.sh
+    rm checkout_all_users.sh
+
+### simple
 create workspace directory (e.g.)
 
     mkdir -p ~/workspace
@@ -27,13 +42,6 @@ download scripts
     wget -nv https://raw.githubusercontent.com/RoboAG/bash_roboag/master/checkout.sh
     source checkout.sh
 
-### all users
-download scripts
-
-    wget -nv https://raw.githubusercontent.com/RoboAG/bash_roboag/master/checkout_all_users.sh
-    source checkout_all_users.sh
-    rm checkout_all_users.sh
-
 ### additional repositories
 checkout additionals repositories (e.g. robolib)
 
@@ -43,13 +51,6 @@ checkout additionals repositories (e.g. robolib)
     ...
     repo_help_overview #list of all referenced repositories
 
-see also [help_setup_workspace.sh](scripts/help/help_setup_workspace.sh).
-
 
 ## Installation
-install git client
-
-    sudo apt install git
-
-
 For install instructions see [doc_install repo](https://github.com/RoboAG/doc_install) (german).
--- scripts/help/help_setup_workspace.sh
+++ scripts/help/help_setup_workspace.sh
@@ -1,35 +1,137 @@
 #!/bin/bash
 
 #***************************[help]********************************************
-# 2020 10 12
+# 2020 12 29
+
+alias robo_help_setup_workspace_shared="robo_help_setup_workspace shared"
+alias robo_help_setup_workspace_simple="robo_help_setup_workspace simple"
 
 function robo_help_setup_workspace() {
 
+    # print help
+    if [ "$1" == "-h" ]; then
+        echo "$FUNCNAME [<mode>]"
+
+        return
+    fi
+    if [ "$1" == "--help" ]; then
+        echo "$FUNCNAME needs 0-1 parameter"
+        echo "    [#1:]mode of the repositories (default: show infos)"
+        echo "           \"shared\"  share workspace with all users"
+        echo "           \"simple\"  setup regular workspace"
+
+        return
+    fi
+
+    # check parameter
+    if [ $# -gt 1 ]; then
+        echo "$FUNCNAME: Parameter Error."
+        $FUNCNAME --help
+        return -1
+    fi
+
+    # check first parameter (mode)
+    mode_flag=""
+    if [ $# -gt 0 ]; then
+        if [ "$1" == "shared" ]; then
+            mode_flag="shared"
+        elif [ "$1" == "simple" ]; then
+            mode_flag="simple"
+        else
+            echo "$FUNCNAME: Parameter Error."
+            $FUNCNAME --help
+            return -1
+        fi
+    fi
+
+    # show general infos
+    if [ "$mode_flag" == "" ]; then
+        echo ""
+        echo "### Config RoboAG Workspace ###"
+        echo ""
+        echo "There are two ways to use our repositories:"
+        echo "  - shared"
+        echo "  - simple"
+        echo ""
+
+        echo "In SHARED mode the repositories are stored globally - for now"
+        echo "the path is \"/opt/roboag/repos/\". Therefore, all users have"
+        echo "access to the repositories. For the setup the current user"
+        echo "needs sudo rights."
+        echo "This is the default setup for our CLIENTS and SERVERS."
+        echo ""
+        echo "See also: $ robo_help_setup_workspace shared"
+        echo ""
+
+        echo "In SIMPLE mode the repositories are stored in a user-defined"
+        echo "place - typically at \"~/workspace/\". This is the simplest"
+        echo "setup, but the repositories are only available to the current"
+        echo "user. This is good for TESTING our scripts or in case you are"
+        echo "having a SINGLE-USER-system working with our repositories."
+        echo ""
+        echo "See also: $ robo_help_setup_workspace simple"
+        echo ""
+
+        return
+    fi
+
+
+    # setup paths
+    if [ "$mode_flag" == "simple" ]; then
+        path_workspace=""
+    else
+        path_workspace="/opt/roboag/repos/"
+    fi
+    path_roboag="${path_workspace}bash/master/roboag/"
+
+
+    # print header
     echo ""
     echo "### Config RoboAG Workspace ###"
     echo ""
-    echo "see also https://github.com/RoboAG/bash_roboag"
+    if [ "$mode_flag" == "simple" ]; then
+        echo "SIMPLE-mode for testing and single-user-systems"
+    else
+        echo "SHARED mode for clients & server"
+    fi
+    echo "See also: $ robo_help_setup_workspace"
     echo ""
+    echo -e "\n<enter>\n"; read dummy
 
-
+    # steps ...
     echo "0. Open Terminal"
-    echo "  <strg>+<alt>+<T>"
+    echo "  <strg>+<alt>+<t>"
     echo -e "\n<enter>\n"; read dummy
 
 
-    echo "1.a) Create working directory"
-    echo "  Recommended is \"~/workspace/\", but any directory is possible!"
-    echo "  $ mkdir -p ~/workspace"
-    echo "  $ cd ~/workspace"
-    echo -e "\n<enter>\n"; read dummy
+    if [ "$mode_flag" == "simple" ]; then
+        echo "1.a) Create working directory"
+        echo "  Recommended is \"~/workspace/\", but any directory is possible!"
+        echo "  $ mkdir -p ~/workspace"
+        echo "  $ cd ~/workspace"
+        echo -e "\n<enter>\n"; read dummy
 
-    echo "1.b) Download scripts"
-    file_part="https://raw.githubusercontent.com/RoboAG/bash_roboag"
-    file="${file_part}/master/checkout.sh"
-    echo "  $ wget -nv ${file}"
-    echo "  $ . checkout.sh"
-    echo "    answer all question with \"y\" (yes)"
+        echo "1.b) Download scripts"
+        filename="checkout.sh"
+    else
+        echo "1.a Download scripts"
+        filename="checkout_all_users.sh"
+    fi
+    url_part1="https://raw.githubusercontent.com/RoboAG/"
+    url_part2="bash_roboag/master/"
+    echo "  $ wget -nv \"${url_part1}${url_part2}${filename}\""
+    echo "  $ source ${filename}"
+    echo "    answer all question with \"yes\""
     echo -e "\n<enter>\n"; read dummy
+    if [ "$mode_flag" != "simple" ]; then
+        echo "1.b) Config settings"
+        echo "  Config files, e.g. changed by scripts or nano_config(),"
+        echo "  will be copied into the shared repositories - this can be a"
+        echo "  SECURITY ISSUE. For our clients this is a feature :-)"
+        echo "  If you want to keep your config files local:"
+        echo "    $ mkdir -p \"\${HOME}/config/\${HOSTNAME}/\""
+        echo -e "\n<enter>\n"; read dummy
+    fi
 
 
     echo "2. Set Mode of Computer"
@@ -41,20 +143,33 @@ function robo_help_setup_workspace() {
     echo ""
     echo "  c) RoboAG server"
     echo "    $ touch \${ROBO_PATH_CONFIG}is_server.txt"
+    echo ""
+    if [ "$mode_flag" == "simple" ]; then
+        echo "  You are following instructions for simple usage, most"
+        echo "  likely a) is your choice."
+    else
+        echo "  You are following instructions for shared usage, most"
+        echo "  likely b) or c) is your choice. Nevertheless, this files"
+        echo "  might also be created later."
+    fi
     echo -e "\n<enter>\n"; read dummy
 
 
     echo "3.a) Source workspace"
-    echo "  If you accepted auto sourcing in step 1.b):"
+    echo "  If you accepted auto sourcing in step 1:"
     echo "    Just close terminal and reopen it."
     echo "  Otherwise, you need to source the downloaded scripts directly:"
-    echo "    $ source ~/workspace/bash/master/roboag/bashrc.sh"
+    echo "    $ source ${path_roboag}bashrc.sh"
+    echo "  Or you can setup auto-sourcing later:"
+    echo "    $ source ${path_roboag}setup_bashrc.sh"
     echo -e "\n<enter>\n"; read dummy
 
     echo "3.b) Download additional repositories"
-    echo "  Only if in standalone-mode, download additional repositories."
+    echo "  If in standalone-mode, download additional repositories."
     echo "  Get all repos        : $ repo_clone_all"
     echo "  Select specific repos: $ repo_clone_bash"
+    echo "                         $ repo_clone_roboag"
+    echo "                         $ repo_clone_robosax"
     echo "                         $ git_clone_... (e.g. git_clone_robo_lib)"
     echo ""
     echo "  For an overview and some hints see:"
