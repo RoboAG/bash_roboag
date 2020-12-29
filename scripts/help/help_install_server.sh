177fa4615558946d965863d99969f44aab61e7c2
Peter Weissig <peterweissig@github.com>
2020-12-29 21:38:24 +0100

    updated help for server install   + simplified choice of devices (only roboag & togo)   + stored old instructions as 18.04   + started to created current instructions for 20.04

--- scripts/help/help_install_server.sh
+++ scripts/help/help_install_server.sh
@@ -16,12 +16,13 @@ function robo_help_install_server() {
         echo "    [#1:]system for which install instructions will be shown"
         echo "         Leave option empty to run for \"roboag\"."
         echo "           \"roboag\"     Server of RoboAG (and RoboSAX)"
-        echo "           \"laptop\"     Peters Laptop"
-        echo "           \"togo\"       Peters ToGo-Server"
-        echo "           \"peter\"      Peters Home-Server"
-        echo "         Deprecated versions:"
-        echo "           \"roboag1604\" Server of RoboAG (Ubuntu 16.04)"
-        echo "           \"togo1604\"   ToGo-Server (Ubuntu 16.04)"
+        echo "           \"togo\"       ToGo-Server"
+        echo "         Deprecated versions for Ubuntu 18.04:"
+        echo "           \"roboag1804\" Server of RoboAG"
+        echo "           \"togo1804\"   ToGo-Server"
+        echo "         Deprecated versions for Ubuntu 16.04:"
+        echo "           \"roboag1604\" Server of RoboAG"
+        echo "           \"togo1604\"   ToGo-Server"
 
         return
     fi
@@ -37,12 +38,20 @@ function robo_help_install_server() {
     system_flag="roboag"
     if [ $# -gt 0 ]; then
         # check for deprecated versions
+        if [ "$1" == "roboag1804" ]; then
+            robo_help_install_server1804 roboag
+            return
+        fi
+        if [ "$1" == "togo1804" ]; then
+            robo_help_install_server1804 togo
+            return
+        fi
         if [ "$1" == "roboag1604" ]; then
             robo_help_install_server1604 roboag
             return
         fi
         if [ "$1" == "togo1604" ]; then
-            robo_help_install_server1604 peter
+            robo_help_install_server1604 togo
             return
         fi
 
@@ -50,10 +59,120 @@ function robo_help_install_server() {
         if [ "$1" == "roboag" ]; then
             # nothing to do :-)
             dummy=1
-        elif [ "$1" == "laptop" ]; then
-            system_flag="laptop"
-        elif [ "$1" == "peter" ]; then
-            system_flag="peter"
+        elif [ "$1" == "togo" ]; then
+            system_flag="togo"
+        else
+            echo "$FUNCNAME: Parameter Error."
+            $FUNCNAME --help
+            return -1
+        fi
+    fi
+
+    echo ""
+    echo "### Install Server ###"
+    echo ""
+    echo "Operating System: Ubuntu Server 20.04.1 LTS"
+    echo ""
+
+    echo "0. Source"
+    echo "  Internet:"
+    echo "    https://releases.ubuntu.com/20.04/"
+    echo -e "\n<enter>\n"; read dummy
+
+
+    echo "1. Willkommen! Bienvenue! Welcome! ..."
+    echo "   ..."
+    echo "   [Català   ]"
+    echo "  *[Deutsch  ]*"
+    echo "   [English  ]"
+    echo "   ..."
+    echo -e "\n<enter>\n"; read dummy
+
+
+    echo "2. Installer-Aktualisierung verfügbar"
+    echo "  ..."
+    echo "   [Aktualisieren auf neuen Installer]"
+    echo "  *[Ohne Aktualisierung fortfahren   ]*"
+    echo "   [Zurück                           ]"
+    echo -e "\n<enter>\n"; read dummy
+
+    echo "3. Tastatur-Konfiguration"
+    echo "  Bitte wählen Sie unten Ihr Tastaturlayout aus ..."
+    echo "  Belegung:  [Deutsch]"
+    echo "  Variante:  [Deutsch]"
+    echo -e "\n<enter>\n"; read dummy
+
+
+    echo "4. Netzwerkverbindungen"
+    echo "  Konfigurieren Sie mindestens eine Schnittstelle ..."
+    echo "  ..."
+    echo "  <nichts zu tun - die Seite listet nur alle Schnittstellen auf>"
+    echo -e "\n<enter>\n"; read dummy
+
+    echo "5. Proxy konfigurieren"
+    echo "  Wenn dieses System einen Proxy erfordert ..."
+    echo "  Proxy address: [  ]"
+    echo "  <nichts zu tun - Proxyadresse leer lassen>"
+    echo -e "\n<enter>\n"; read dummy
+
+    echo "6. Konfiguriere Ubuntu-Archive-Mirror"
+    echo "  Wenn Sie einen ... Spiegelserver für Ubuntu verwenden ..."
+    echo "  Mirror-Adresse: [http://de.archive.ubuntu.com/ubuntu]"
+    echo "  <nichts zu tun - Mirror-Adresse nicht verändern>"
+    echo -e "\n<enter>\n"; read dummy
+
+
+    echo "7. Begleitete Speicherplatzkonfiguration"
+    echo "  Configure a guided storage layout, or create a custom one:"
+    echo "  [ ] Eine ganze Festplatte verwenden"
+    echo "      ..."
+    echo "  [x] Custom storage layout"
+    echo -e "\n<enter>\n"; read dummy
+
+    echo "8.a) Speicherplatzkonfiguration"
+    echo "  ..."
+    echo "  TODO"
+    echo "  ..."
+    echo -e "\n<enter>\n"; read dummy
+
+    echo "done :-)"
+}
+
+#***************************[18.04]*******************************************
+# 2020 12 29
+
+function robo_help_install_server1804() {
+
+    # print help
+    if [ "$1" == "-h" ]; then
+        echo "$FUNCNAME [<system>]"
+
+        return
+    fi
+    if [ "$1" == "--help" ]; then
+        echo "$FUNCNAME needs 0-1 parameters"
+        echo "    [#1:]system for which install instructions will be shown"
+        echo "         Leave option empty to run for \"roboag\"."
+        echo "           \"roboag\"  Server of RoboAG (and RoboSAX)"
+        echo "           \"togo\"    ToGo-Server"
+
+        return
+    fi
+
+    # check parameter
+    if [ $# -gt 1 ]; then
+        echo "$FUNCNAME: Parameter Error."
+        $FUNCNAME --help
+        return -1
+    fi
+
+    # check first parameter (system-flag)
+    system_flag="roboag"
+    if [ $# -gt 0 ]; then
+        #check for current versions
+        if [ "$1" == "roboag" ]; then
+            # nothing to do :-)
+            dummy=1
         elif [ "$1" == "togo" ]; then
             system_flag="togo"
         else
@@ -104,9 +223,6 @@ function robo_help_install_server() {
     echo "  Configure at least one interface this server can use ..."
     if [ "$system_flag" == "roboag" ]; then
         echo "    enp63s0"
-    elif [ "$system_flag" == "laptop" ] || \
-      [ "$system_flag" == "peter" ]; then
-        echo "    enp4s0"
     else
         echo "    enp1s0"
         echo "    enp4s0"
@@ -134,47 +250,20 @@ function robo_help_install_server() {
     echo -e "\n<enter>\n"; read dummy
 
     echo "8.a) Dateisystem einrichten (7/11)"
-    if [ "$system_flag" == "laptop" ]; then
-        echo "  SSD:"
-        echo "    a) System"
-        echo "       Size (max. 476G)   : 50G"
-        echo "       Format             : ext4"
-        echo "       Mount              : /"
-        echo "    b) Swap"
-        echo "       Size (max. 426G)   : 10G"
-        echo "       Format             : swap"
-        echo "       Mount              : SWAP"
-        echo "    c) Home"
-        echo "       Size (max. 416G)   : 300G        <ca. 2/3>"
-        echo "       Format             : ext4"
-        echo "       Mount              : /home"
-        echo "    d) Home"
-        echo "       Size (max. 416G)   :             <leave empty>"
-        echo "       Format             : ext4"
-        echo "       Mount              : /media/data"
-        echo -e "\n<enter>\n"; read dummy
-
-        echo "FILE SYSTEM SUMMARY"
-        echo "  /               50G  ext4   ..."
-        echo "  /home          300G  ext4   ..."
-        echo "  /media/data    116G  ext4   ..."
-        echo "  SWAP            10G  swap   ..."
-    else # for roboag, peter and togo
-        echo "  SSD:"
-        echo "    a) System"
-        echo "       Size (max. 111G)   : 50G         <ca. 45%>"
-        echo "       Format             : ext4"
-        echo "       Mount              : /"
-        echo "    b) Swap"
-        echo "       Size (max.  61G)   : 10G"
-        echo "       Format             : swap"
-        echo "       Mount              : SWAP"
-        echo "    c) Home"
-        echo "       Size (max.  51G)   :             <leave empty>"
-        echo "       Format             : ext4"
-        echo "       Mount              : /home"
-        echo -e "\n<enter>\n"; read dummy
-    fi
+    echo "  SSD:"
+    echo "    a) System"
+    echo "       Size (max. 111G)   : 50G         <ca. 45%>"
+    echo "       Format             : ext4"
+    echo "       Mount              : /"
+    echo "    b) Swap"
+    echo "       Size (max.  61G)   : 10G"
+    echo "       Format             : swap"
+    echo "       Mount              : SWAP"
+    echo "    c) Home"
+    echo "       Size (max.  51G)   :             <leave empty>"
+    echo "       Format             : ext4"
+    echo "       Mount              : /home"
+    echo -e "\n<enter>\n"; read dummy
 
     if [ "$system_flag" == "roboag" ]; then
         echo "  HDD:"
@@ -194,25 +283,6 @@ function robo_help_install_server() {
         echo "  /home           51G  ext4   ..."
         echo "  /media/data     50G  ext4   ..."
         echo "  /media/backup  182G  ext4   ..."
-        echo "  SWAP            10G  swap   ..."
-    elif [ "$system_flag" == "peter" ]; then
-        echo "  HDD:"
-        echo "    a) Data"
-        echo "       Size (max. 931G)   :             <leave empty>"
-        echo "       Format             : ext4"
-        echo "       Mount              : /mnt/hdd"
-        echo -e "\n<enter>\n"; read dummy
-
-        echo "  RAID:"
-        echo "    Setting up the raid here didn't work at all."
-        echo "    No configuration to be done!"
-        echo -e "\n<enter>\n"; read dummy
-
-        echo "FILE SYSTEM SUMMARY"
-        echo "  /               50G  ext4   ..."
-        echo "  /boot/efi      512M  fat32  ..."
-        echo "  /home           51G  ext4   ..."
-        echo "  /mnt/hdd       931G  ext4   ..."
         echo "  SWAP            10G  swap   ..."
     else
         echo "  HDD:"
@@ -261,18 +331,10 @@ function robo_help_install_server() {
         echo "  Your name             : Guru der RoboAG"
         echo "  Your server's name    : server"
         echo "  Pick a username       : guru"
-    elif [ "$system_flag" == "laptop" ]; then
-        echo "  Your name             : Peter"
-        echo "  Your server's name    : atmega"
-        echo "  Pick a username       : peter"
-    elif [ "$system_flag" == "peter" ]; then
-        echo "  Your name             : Peter"
-        echo "  Your server's name    : flunder"
-        echo "  Pick a username       : peter"
     else
-        echo "  Your name             : Peter"
+        echo "  Your name             : Peter (beliebig)"
         echo "  Your server's name    : blackbox"
-        echo "  Pick a username       : peter"
+        echo "  Pick a username       : peter (beliebig)"
     fi
     echo "  Choose a password     : xxx"
     echo "  Confirm your password : xxx"
@@ -281,14 +343,10 @@ function robo_help_install_server() {
 
     echo "10. Featured Server Snaps (9/11)"
     echo "  These are popular snaps in server environments. ..."
-    if [ "$system_flag" == "roboag" ] || \
-      [ "$system_flag" == "laptop" ]; then
+    if [ "$system_flag" == "roboag" ]; then
         echo "  [ ] ..."
         echo "  <leave all options unchecked>"
     else
-        # peter and togo
-        echo "  [ ] ..."
-        echo "  [x] nextcloud"
         echo "  [ ] ..."
         echo "  [x] minidlna-escoand"
     fi
@@ -321,18 +379,18 @@ function robo_help_install_server() {
     echo "  $ sudo reboot"
     echo -e "\n<enter>\n"; read dummy
 
-    if [ "$system_flag" == "roboag" ] || [ "$system_flag" == "peter" ]; then
-        echo "14. Config"
-        echo "  for further configuration and setup see"
-        echo "  $ robo_help_setup_workspace"
-        echo "  $ robo_help_setup server"
-    fi
+    echo "14. Config"
+    echo "  for further configuration and setup see"
+    echo "  $ robo_help_setup_workspace"
+    echo "  $ robo_help_setup server"
 
     echo "done :-)"
 }
 
-#***************************[16.04.]******************************************
-# 2018 02 18
+
+
+#***************************[16.04]*******************************************
+# 2020 12 29
 
 function robo_help_install_server1604() {
 
@@ -346,8 +404,8 @@ function robo_help_install_server1604() {
         echo "$FUNCNAME needs 0-1 parameters"
         echo "    [#1:]system for which install instructions will be shown"
         echo "         Leave option empty to run for \"roboag\"."
-        echo "           \"roboag\"     Server of RoboAG (and RoboSAX)"
-        echo "           \"peter\"      Peter's ToGo-Server"
+        echo "           \"roboag\"  Server of RoboAG (and RoboSAX)"
+        echo "           \"togo\"    ToGo-Server"
 
         return
     fi
@@ -365,8 +423,8 @@ function robo_help_install_server1604() {
         if [ "$1" == "roboag" ]; then
             # nothing to do :-)
             dummy=1
-        elif [ "$1" == "peter" ]; then
-            system_flag="peter"
+        elif [ "$1" == "togo" ]; then
+            system_flag="togo"
         else
             echo "$FUNCNAME: Parameter Error."
             $FUNCNAME --help
@@ -436,11 +494,7 @@ function robo_help_install_server1604() {
 
     echo "6. Netzwerk einrichten"
     echo "  Rechnername:"
-    echo "  [?]"
-    echo "    <vergeben   : Server   (RoboAG),"
-    echo "                  blackbox (peter) ,"
-    echo "                  flunder  (peter) >"
-    echo "    <noch offen : ?                >"
+    echo "  [server]"
     echo -e "\n<enter>\n"; read dummy
 
 
@@ -449,7 +503,7 @@ function robo_help_install_server1604() {
     if [ "$system_flag" == "roboag" ]; then
         echo "  [Guru]"
     else
-        echo "  [Peter]"
+        echo "  [Peter] (beliebig)"
     fi
     echo -e "\n<enter>\n"; read dummy
 
@@ -458,7 +512,7 @@ function robo_help_install_server1604() {
     if [ "$system_flag" == "roboag" ]; then
         echo "  [guru]"
     else
-        echo "  [peter]"
+        echo "  [peter] (beliebig)"
     fi
     echo -e "\n<enter>\n"; read dummy
 
@@ -630,8 +684,8 @@ function robo_help_install_server1604() {
 
     echo "Reboot"
     echo "  log into server"
-    echo "  $ sudo apt-get update"
-    echo "  $ sudo apt-get install ubuntu-desktop"
+    echo "  $ sudo apt update"
+    echo "  $ sudo apt install ubuntu-desktop"
     echo -e "\n<enter>\n"; read dummy
 
     echo "done :-)"
