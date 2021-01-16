d12ead9ce2b6db1159016e40f4719b08cfa0ac73
Peter Weissig <peterweissig@github.com>
2021-01-16 22:58:43 +0100

    updated robo_help_setup_workspace() to support "client"   (client mode is like shared mode, but specialised to be used within server environment)

--- scripts/help/help_setup_workspace.sh
+++ scripts/help/help_setup_workspace.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #***************************[help]********************************************
-# 2021 01 12
+# 2021 01 16
 
 alias robo_help_setup_workspace_shared="robo_help_setup_workspace shared"
+alias robo_help_setup_workspace_client="robo_help_setup_workspace client"
 alias robo_help_setup_workspace_simple="robo_help_setup_workspace simple"
 
 function robo_help_setup_workspace() {
@@ -18,6 +19,7 @@ function robo_help_setup_workspace() {
         echo "$FUNCNAME needs 0-1 parameter"
         echo "    [#1:]mode of the repositories (default: show infos)"
         echo "           \"shared\"  share workspace with all users"
+        echo "           \"client\"  like \"shared\", but relying on server"
         echo "           \"simple\"  setup regular workspace"
 
         return
@@ -35,6 +37,8 @@ function robo_help_setup_workspace() {
     if [ $# -gt 0 ]; then
         if [ "$1" == "shared" ]; then
             mode_flag="shared"
+        elif [ "$1" == "client" ]; then
+            mode_flag="client"
         elif [ "$1" == "simple" ]; then
             mode_flag="simple"
         else
@@ -55,12 +59,21 @@ function robo_help_setup_workspace() {
         echo ""
 
         echo "In SHARED mode the repositories are stored globally - for now"
-        echo "the path is \"/opt/roboag/repos/\". Therefore, all users have"
+        echo -n "the path is \"${ROBO_PATH_SHARED_REPOS}\". "
+        echo    "Therefore, all users have"
         echo "access to the repositories. For the setup the current user"
         echo "needs sudo rights."
         echo "This is the default setup for our CLIENTS and SERVERS."
         echo ""
         echo "See also: $ robo_help_setup_workspace_shared"
+        echo ""
+
+        echo "CLIENT mode is very similar to shared mode. This mode works"
+        echo "without direct access to internet. However, the server needs"
+        echo "to be running. Therefore, this should be the first choice,"
+        echo "if a client is setup within RoboAG."
+        echo ""
+        echo "See also: $ robo_help_setup_workspace_client"
         echo ""
 
         echo "In SIMPLE mode the repositories are stored in a user-defined"
@@ -80,7 +93,7 @@ function robo_help_setup_workspace() {
     if [ "$mode_flag" == "simple" ]; then
         path_workspace=""
     else
-        path_workspace="/opt/roboag/repos/"
+        path_workspace="$ROBO_PATH_SHARED_REPOS"
     fi
     path_roboag="${path_workspace}bash/master/roboag/"
 
@@ -89,10 +102,12 @@ function robo_help_setup_workspace() {
     echo ""
     echo "### Config RoboAG Workspace ###"
     echo ""
-    if [ "$mode_flag" == "simple" ]; then
-        echo "SIMPLE-mode for testing and single-user-systems"
-    else
+    if [ "$mode_flag" == "shared" ]; then
         echo "SHARED mode for clients & server"
+    elif [ "$mode_flag" == "client" ]; then
+        echo "CLIENT mode based on running server"
+    else
+        echo "SIMPLE-mode for testing and single-user-systems"
     fi
     echo "See also: $ robo_help_setup_workspace"
     echo ""
@@ -104,87 +119,108 @@ function robo_help_setup_workspace() {
     echo -e "\n<enter>\n"; read dummy
 
 
-    if [ "$mode_flag" == "simple" ]; then
-        echo "1.a) Create working directory"
-        echo "  Recommended is \"~/workspace/\", but any directory is possible!"
-        echo "  $ mkdir -p ~/workspace"
-        echo "  $ cd ~/workspace"
+    if [ "$mode_flag" != "client" ]; then
+        if [ "$mode_flag" == "shared" ]; then
+            echo "1.a) Download scripts"
+            filename="checkout_all_users.sh"
+        else
+            echo "1.a) Create working directory"
+            echo -n "  Recommended is \"~/workspace/\", "
+            echo    "but any directory is possible!"
+            echo "  $ mkdir -p ~/workspace"
+            echo "  $ cd ~/workspace"
+            echo -e "\n<enter>\n"; read dummy
+
+            echo "1.b) Download scripts"
+            filename="checkout.sh"
+        fi
+        url_part1="https://raw.githubusercontent.com/RoboAG/"
+        url_part2="bash_roboag/master/"
+        echo "  $ wget -nv \"${url_part1}${url_part2}${filename}\""
+        echo "  $ source ${filename}"
+        if [ "$mode_flag" == "simple" ]; then
+            echo "    answer all questions with \"yes\""
+        else
+            echo "    answer question with \"yes\""
+        fi
+        echo -e "\n<enter>\n"; read dummy
+        if [ "$mode_flag" != "simple" ]; then
+            echo "1.b) Remove checkout script"
+            echo "  $ rm \"${filename}\""
+            echo -e "\n<enter>\n"; read dummy
+
+            echo "1.c) Config settings"
+            echo "  Config files, e.g. changed by scripts or nano_config(),"
+            echo "  will be copied into the shared repositories - this can be"
+            echo "  a SECURITY ISSUE. For our clients this is a feature :-)"
+            echo "  If you want to keep your config files local:"
+            echo "    $ mkdir -p \"\${HOME}/config/\${HOSTNAME}/\""
+            echo -e "\n<enter>\n"; read dummy
+        fi
+    else # client mode
+        echo "1.a) Create shared folder"
+        echo "  $ sudo mkdir -p \"$ROBO_PATH_SHARED_REPOS\""
+        echo "  $ sudo chown \$USER \"$ROBO_PATH_SHARED_REPOS\""
         echo -e "\n<enter>\n"; read dummy
 
-        echo "1.b) Download scripts"
-        filename="checkout.sh"
-    else
-        echo "1.a) Download scripts"
-        filename="checkout_all_users.sh"
-    fi
-    url_part1="https://raw.githubusercontent.com/RoboAG/"
-    url_part2="bash_roboag/master/"
-    echo "  $ wget -nv \"${url_part1}${url_part2}${filename}\""
-    echo "  $ source ${filename}"
-    if [ "$mode_flag" == "simple" ]; then
-        echo "    answer all questions with \"yes\""
-    else
-        echo "    answer question with \"yes\""
-    fi
-    echo -e "\n<enter>\n"; read dummy
-    if [ "$mode_flag" != "simple" ]; then
-        echo "1.b) Remove checkout script"
-        echo "  $ rm \"${filename}\""
-        echo -e "\n<enter>\n"; read dummy
-
-        echo "1.c) Config settings"
-        echo "  Config files, e.g. changed by scripts or nano_config(),"
-        echo "  will be copied into the shared repositories - this can be a"
-        echo "  SECURITY ISSUE. For our clients this is a feature :-)"
-        echo "  If you want to keep your config files local:"
-        echo "    $ mkdir -p \"\${HOME}/config/\${HOSTNAME}/\""
+        echo "1.b) Copy scripts from server"
+        echo "  $ cd \"$ROBO_PATH_SHARED_REPOS\""
+        temp="${ROBO_PATH_SHARED_REPOS}bash/"
+        echo "  $ scp -r guru@${_ROBO_SERVER_IP}:${temp} ."
         echo -e "\n<enter>\n"; read dummy
     fi
 
 
     echo "2. Source workspace"
-    echo "  If you accepted auto sourcing in step 1:"
-    echo "    Just close terminal and reopen it."
-    echo "  Otherwise, you need to source the downloaded scripts directly:"
-    echo "    $ source ${path_roboag}bashrc.sh"
-    echo "  Or you can setup auto-sourcing later:"
+    if [ "$mode_flag" != "client" ]; then
+        echo "  If you did not accept auto sourcing in step 1:"
+        echo "  You need to source the downloaded scripts directly:"
+        echo "    $ source ${path_roboag}bashrc.sh"
+        echo "  Or you can setup auto-sourcing later:"
+    fi
     echo "    $ source ${path_roboag}setup_bashrc.sh"
-    echo "    # remember to close and reopen terminal afterwards"
+    echo "    # remember to answer question with \"yes\""
     echo -e "\n<enter>\n"; read dummy
 
 
     echo "3. Set Mode of Computer"
-    echo "  a) Standalone client (no server available)"
-    echo "    nothing todo"
-    echo ""
-    echo "  b) Client within RoboAG (connection to RoboAG server)"
+    if [ "$mode_flag" != "client" ]; then
+        echo "  a) Standalone client (no server available)"
+        echo "    nothing todo"
+        echo ""
+        echo "  b) Client within RoboAG (connection to RoboAG server)"
+    fi
     echo "    $ robo_config_mode_set client"
-    echo ""
-    echo "  c) RoboAG server"
-    echo "    $ robo_config_mode_set server"
-    echo ""
-    if [ "$mode_flag" == "simple" ]; then
-        echo "  You are following instructions for simple usage, most"
-        echo "  likely a) is your choice."
-    else
-        echo "  You are following instructions for shared usage, most"
-        echo "  likely b) or c) is your choice. Nevertheless, this files"
-        echo "  might also be created later."
+    if [ "$mode_flag" != "client" ]; then
+        echo ""
+        echo "  c) RoboAG server"
+        echo "    $ robo_config_mode_set server"
+        echo ""
+        if [ "$mode_flag" == "simple" ]; then
+            echo "  You are following instructions for simple usage, most"
+            echo "  likely a) is your choice."
+        else
+            echo "  You are following instructions for shared usage, most"
+            echo "  likely b) or c) is your choice. Nevertheless, this files"
+            echo "  might also be created later."
+        fi
     fi
     echo ""
     echo -e "\n<enter>\n"; read dummy
 
-    echo "4. Download additional repositories"
-    echo "  If in standalone-mode, download additional repositories."
-    echo "  Get all repos        : $ repo_clone_all"
-    echo "  Select specific repos: $ repo_clone_bash"
-    echo "                         $ repo_clone_roboag"
-    echo "                         $ repo_clone_robosax"
-    echo "                         $ git_clone_... (e.g. git_clone_robo_lib)"
-    echo ""
-    echo "  For an overview and some hints see:"
-    echo "    $ robo_repo_overview"
-    echo -e "\n<enter>\n"; read dummy
+    if [ "$mode_flag" != "client" ]; then
+        echo "4. Download additional repositories"
+        echo "  Get all repos        : $ repo_clone_all"
+        echo "  Select specific repos: $ repo_clone_bash"
+        echo "                         $ repo_clone_roboag"
+        echo "                         $ repo_clone_robosax"
+        echo -n "                         $ git_clone_... "
+        echo    "(e.g. git_clone_robo_lib)"
+        echo ""
+        echo "  For an overview and some hints see:"
+        echo "    $ robo_repo_overview"
+        echo -e "\n<enter>\n"; read dummy
+    fi
 
 
     echo "Further setup instructions can be found here:"
