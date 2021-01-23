0a71008827b09ae955a0614b0d426b1afbdaad38
Peter Weissig <peterweissig@github.com>
2021-01-23 14:09:00 +0100

    updated robo_help_daily()   + added robo_help_setup_workspace   + fixed ssh command for server (robo_server_ssh)   + added dhcp list for server (robo_config_server_dhcp_show)

--- scripts/help/help_daily.sh
+++ scripts/help/help_daily.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #***************************[help]********************************************
-# 2021 01 19
+# 2021 01 23
 
 function robo_help_daily() {
 
@@ -28,28 +28,25 @@ function robo_help_daily() {
     fi
 
     echo "help"
-    echo "  $ robo_help           # overview"
-    echo "  $ robo_help_daily     # this script"
-    echo "  $ robo_help_install   # install"
-    echo "  $ robo_help_setup     # setup"
+    echo "  $ robo_help                 # overview"
+    echo "  $ robo_help_daily           # this script"
+    echo "  $ robo_help_install         # install"
+    echo "  $ robo_help_setup_workspace # copy scripts"
+    echo "  $ robo_help_setup           # setup"
     echo ""
 
     echo "system"
-    echo "  $ robo_system_wtf     # check  system"
-    echo "  $ robo_repo_update    # update repos"
-    if [ "$ROBO_CONFIG_IS_CLIENT" == "1" ]; then
-        echo "  $ robo_repo_clone_from_server # clone repos"
-    fi
-    echo "  $ robo_system_update  # update system"
-    if [ "$ROBO_CONFIG_IS_SERVER" == "1" ]; then
-        echo "  $ robo_system_ssh     # ssh into each client"
-    fi
+    echo "  $ robo_system_wtf           # check  system"
+    echo "  $ robo_repo_update          # update repos"
+    echo "  $ robo_system_update        # update system"
     echo ""
 
     if [ "$ROBO_CONFIG_IS_SERVER" == "1" ]; then
         echo "clients"
-        echo "  $ robo_server_ssh_update # update their system & repos"
-        echo "  $ robo_server_ssh_check  # check  their system"
+        echo "  $ robo_config_server_dhcp_show # list clients"
+        echo "  $ robo_server_ssh              # ssh into each client"
+        echo "  $ robo_server_ssh_update       # update their system & repos"
+        echo "  $ robo_server_ssh_check        # check  their system"
         echo ""
         echo "server"
         echo "  $ robo_config_server_internet_on  # internet ON"
