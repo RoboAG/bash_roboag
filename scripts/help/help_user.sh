45b79e6db695b80ef8213c44e8a9af9b24d0b5f9
Peter Weissig <peterweissig@github.com>
2022-02-11 20:10:46 +0100

    updated help file for user
    + replacing robolib_all with robolib_build
    + added info about optional comport-parameter for robolib_data

--- scripts/help/help_user.sh
+++ scripts/help/help_user.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 #***************************[help]********************************************
-# 2021 08 12
+# 2022 02 11
 
 function robo_help_user() {
 
     echo "### ROBOAG ###"
     echo ""
 
-    echo "  $ robolib_init_kepler # Legt die Konfig für das Keplerboard an"
-    echo "  $ robolib_init_3pi    # Legt die Konfig für einen 3Pi an"
+    echo "  $ robolib_init_kepler    # Legt die Konfig für das Keplerboard an"
+    echo "  $ robolib_init_3pi       # Legt die Konfig für einen 3Pi an"
     echo ""
-    echo "  $ robolib_all         # Kompiliert im aktuellen Ordner"
-    echo "  $ robolib_clean       # Entfernt alle Kompilierartefakte"
+    echo "  $ robolib_build          # Kompiliert im aktuellen Ordner"
+    echo "  $ robolib_clean          # Entfernt alle Kompilierartefakte"
     echo ""
-    echo "  $ robolib_avrdude     # Avrdude zum Herunterladen (3Pi, ISP)"
-    echo "  $ robolib_download    # Robolib-Downloader (RS232 & Bootloader)"
+    echo "  $ robolib_avrdude        # Avrdude zum Herunterladen (3Pi, ISP)"
+    echo "  $ robolib_download       # Robolib-Downloader (RS232 & Bootloader)"
     echo ""
-    echo "  $ robolib_data        # Einfacher Datanaustausch (RS232)"
+    echo "  $ robolib_data [comport] # Einfacher Datanaustausch (RS232)"
     echo ""
 }
